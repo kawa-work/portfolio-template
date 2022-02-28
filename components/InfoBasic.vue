d413d83d372d72bcbacb2c8c4f73c5106e7029fa
kawa-work <k.kawakami.work@gmail.com>
2022-02-28 20:48:00 +0900

    refactor: put basic info data in content directory

--- components/InfoBasic.vue
+++ components/InfoBasic.vue
@@ -18,23 +18,11 @@
 
 <script>
 export default {
-  data() {
-    return {
-      info: [
-        {
-          name: '名前',
-          val: 'サンプル名前',
-        },
-        {
-          name: '年齢',
-          val: 'サンプル年齢',
-        },
-        {
-          name: '希望職種',
-          val: 'サンプル職種',
-        },
-      ],
-    }
+  data: () => ({
+    info: [],
+  }),
+  async fetch() {
+    this.info = await this.$content('basic').sortBy("createdAt").fetch()
   },
 }
 </script>
